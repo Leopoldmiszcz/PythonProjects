1cf6548611aeefef86c2bd5727a91dde79edb2eb
Leopold Kaniewski <leopoldmiszcz@outlook.com>
2023-07-10 20:34:12 +0200

    QRcode generator version 1.0.0

--- qrcode/main.py
+++ qrcode/main.py
@@ -1,5 +1,3 @@
-#  TODO:
-
 import qrcode
 
 qr = qrcode.QRCode(
@@ -9,61 +7,92 @@ qr = qrcode.QRCode(
     border=4,
 )
 
-colors = {'red': '225, 225, 225',
-          'orange': '255, 128, 0',
-          'yellow': '255, 255, 0',
-          'green': '0, 255, 0',
-          'light-blue': '0, 255, 255',
-          'blue': '0, 0, 255',
-          'purple': '128, 0, 255',
-          'pink': '255, 0, 255',
-          'magenta': '255, 0, 191'}
 
-print(colors['red'])
+colors = ['red', 'orange', 'yellow', 'green', 'light-blue', 'blue', 'purple', 'pink', 'magenta']
+
 
 print("Hello! Welcome to qrcode generator!!!")
-link = input("Please insert a link: ")
-name = input("Please name your qr code: ")
-print("Thank you!")
+
+link = ""
+
+while len(link) == 0:
+    link = input("Please insert a link: ")
+
+name = ""
+
+while len(name) == 0:
+    name = input("Please name your qr code: ")
+
 print("Do you want to change qrcode color? (by default it is white and black)")
-colorchange = input("Y/n: ")
-colorchange = colorchange.upper()
+
+color_change = input("Y/n: ")
+color_change = color_change.upper()
 
 color = None
 
 
-if colorchange == "Y":
+if color_change == "Y":
     while color not in colors:  # ta pentla każe wybrać kolor, jak ktoś nie wybierze to zapentla
         print("Available colors: red, orange, yellow, green, light-blue,\nblue, purple, pink, magenta")
         color = input("Pick a color: ").lower()
-        rgb_value = None
 
-    if color == "red":
-        rgb_value == 255, 255, 255
+    if color == "red":  # red
+        qrcode = qr.make_image(back_color=(255, 255, 255), fill_color=(255, 0, 0))
+        qr.add_data(link)
+        qrcode.save(name + '.png')
 
-    # match color:
-    #     case "red":
-    #         rgb_value = 255, 255, 255
-    #     case "orange":
-    #         rgb_value = 255, 128, 0
-    #     case "yellow":
-    #         rgb_value = 255, 255, 0
-    #     case "green":
-    #         rgb_value = 0, 255, 0
-    #     case "light-blue":
-    #         rgb_value = 0, 255, 255
-    #     case "blue":
-    #         rgb_value = 0, 0, 255
-    #     case "purple":
-    #         rgb_value = 128, 0, 255
-    #     case "pink":
-    #         rgb_value = 255, 0, 255
-    #     case "magenta":
-    #         rgb_value = 255, 0, 191
-    #
+    elif color == "orange":  # orange
+        qr.add_data(link)
+        qr.make(fit=True)
+        qrcode = qr.make_image(back_color=(255, 255, 255), fill_color=(255, 128, 0))
+        qrcode.save(name + '.png')
 
-    qrcode = qr.make_image(back_color=(rgb_value), fill_color=(rgb_value))
+    elif color == "yellow":  # yellow
+        qr.add_data(link)
+        qr.make(fit=True)
+        qrcode = qr.make_image(back_color=(255, 255, 255), fill_color=(255, 255, 0))
+        qrcode.save(name + '.png')
+
+    elif color == "green":  # green
+        qr.add_data(link)
+        qr.make(fit=True)
+        qrcode = qr.make_image(back_color=(255, 255, 255), fill_color=(0, 255, 0))
+        qrcode.save(name + '.png')
+
+    elif color == "light-blue":  # light-blue
+        qr.add_data(link)
+        qr.make(fit=True)
+        qrcode = qr.make_image(back_color=(255, 255, 255), fill_color=(0, 255, 255))
+        qrcode.save(name + '.png')
+
+    elif color == "blue":  # blue
+        qr.add_data(link)
+        qr.make(fit=True)
+        qrcode = qr.make_image(back_color=(255, 255, 255), fill_color=(0, 0, 255))
+        qrcode.save(name + '.png')
+
+    elif color == "purple":  # purple
+        qr.add_data(link)
+        qr.make(fit=True)
+        qrcode = qr.make_image(back_color=(255, 255, 255), fill_color=(128, 0, 255))
+        qrcode.save(name + '.png')
+
+    elif color == "pink":  # pink
+        qr.add_data(link)
+        qr.make(fit=True)
+        qrcode = qr.make_image(back_color=(255, 255, 255), fill_color=(255, 0, 255))
+        qrcode.save(name + '.png')
+
+    elif color == "magenta":  # magenta
+        qr.add_data(link)
+        qr.make(fit=True)
+        qrcode = qr.make_image(back_color=(255, 255, 255), fill_color=(255, 0, 191))
+        qrcode.save(name + '.png')
+
+else:  # basic
+    qr.add_data(link)
+    qr.make(fit=True)
+    qrcode = qr.make_image(back_color=(255, 255, 255), fill_color=(0, 0, 0))
     qrcode.save(name + '.png')
-else:
-    qrcode = qr.make(link)
-    qrcode.save(name + '.png')
+
+print("Thank you for using our qrcode generator!")
